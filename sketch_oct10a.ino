f4fd08763dd690279524f574fd40d72b6060ecfb
Robert Quitt <robertquitt@gmail.com>
2016-11-07 21:21:31 -0800

    Update pin numbers

--- sketch_oct10a.ino
+++ sketch_oct10a.ino
@@ -1,12 +1,12 @@
 #define LMF_PIN 9
 #define LMR_PIN 10
-#define RMF_PIN PD5
-#define RMR_PIN PD6
+#define RMF_PIN 5
+#define RMR_PIN 6
 
-#define LE1_PIN 12
-#define LE2_PIN 13
-#define RE1_PIN 1
-#define RE2_PIN 2
+#define LE1_PIN 3
+#define LE2_PIN 8
+#define RE1_PIN 2
+#define RE2_PIN 7
 
 volatile int leftTicks = 0;
 volatile int rightTicks = 0;
@@ -20,26 +20,25 @@ void setup() {
   pinMode(LE2_PIN, INPUT);
   pinMode(RE1_PIN, INPUT);
   pinMode(RE2_PIN, INPUT);
+  Serial.begin(9600);
   attachInterrupt(LE1_PIN, onLeftTick, FALLING);
   attachInterrupt(RE1_PIN, onRightTick, FALLING);
-  Serial.begin(9600);
-  
 }
 
 void loop() {
-  motorLeft(0.5);
-  motorRight(0.5);
-  delay(500);
-  motorLeft(0);
-  motorRight(0);
-  delay(500);
-  motorLeft(-0.5);
-  motorRight(-0.5);
-  delay(500);
-  motorLeft(0);
-  motorRight(0);
-  delay(500);
-  Serial.println(leftTicks, rightTicks);
+//  motorLeft(0.5);
+//  motorRight(0.5);
+//  delay(500);
+//  motorLeft(0);
+//  motorRight(0);
+//  delay(500);
+//  motorLeft(-0.5);
+//  motorRight(-0.5);
+//  delay(500);
+//  motorLeft(0);
+//  motorRight(0);
+//  delay(500);
+//  Serial.println(leftTicks);
 }
 
 void onLeftTick() {
@@ -48,6 +47,8 @@ void onLeftTick() {
   } else {
     leftTicks--;
   }
+  Serial.print("Left: ");
+  Serial.println(leftTicks);
 }
 
 void onRightTick() {
@@ -56,6 +57,8 @@ void onRightTick() {
   } else {
     rightTicks--;
   }
+  Serial.print("Right: ");
+  Serial.println(rightTicks);
 }
 
 void motorLeft(float spd) {
