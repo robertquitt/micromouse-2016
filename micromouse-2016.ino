865fc8d5a1c6d5145e1cbf0949292dec8fda6975
Robert Quitt <robertquitt@berkeley.edu>
2016-12-05 14:39:35 -0800

    Update Pins and motor logic

--- micromouse-2016.ino
+++ micromouse-2016.ino
@@ -1,17 +1,26 @@
-#define LMF_PIN 9
-#define LMR_PIN 10
-#define RMF_PIN 5
-#define RMR_PIN 6
+#define LMF_PIN 7
+#define LMR_PIN 8
+#define LMPWM_PIN 6
+#define RMF_PIN 10
+#define RMR_PIN 11
+#define RMPWM_PIN 9
 
 #define LE1_PIN 3
-#define LE2_PIN 8
+#define LE2_PIN 5
 #define RE1_PIN 2
-#define RE2_PIN 7
+#define RE2_PIN 4
 
 #define Apin 14  // Analog battery monitoring pin
 
-volatile int leftTicks = 0;
-volatile int rightTicks = 0;
+#define NUMREADINGS 10
+#define LOOPTIME 100
+
+const float Kp = 0.4;
+const float Kd = 1;
+const float Ki = 0.1;
+int readings[NUMREADINGS];
+volatile long leftTicks = 0;
+volatile long rightTicks = 0;
 
 
 
@@ -24,79 +33,146 @@ void setup() {
   pinMode(LE2_PIN, INPUT);
   pinMode(RE1_PIN, INPUT);
   pinMode(RE2_PIN, INPUT);
+//  for(int i=0; i<NUMREADINGS; i++)
+//    readings[i] = 0;
   Serial.begin(9600);
   attachInterrupt(digitalPinToInterrupt(LE1_PIN), onLeftTick, FALLING);
   attachInterrupt(digitalPinToInterrupt(RE1_PIN), onRightTick, FALLING);
 }
 
+unsigned long lastMilli = 0;
+int lastLeftError = 0;
+int lastRightError = 0;
+int target = 300;
+int leftI = 0;
+int rightI = 0;
+
 void loop() {
-//  for (int i = 0; i < 255; i++) {
-//    motorLeft(i);
-//    motorRight(i);
-//    Serial.println(i);
-//    Serial.println(leftTicks);
-//    Serial.println(rightTicks);
-//    delay(20);
+  if (millis() - lastMilli >= LOOPTIME) {
+    motorLeft(updateLeft(0, 500, leftTicks));
+    motorRight(updateRight(0, 500, rightTicks));
+  }
+  
+//  if (rightTicks < 300) {
+//    motorRight(127);
+//  } else if (rightTicks > 310){
+//    motorRight(-127);
+//  } else {
+//    motorRight(0);
 //  }
-  if (rightTicks < 300) {
-    motorRight(127);
-  } else if (rightTicks > 310){
-    motorRight(-127);
+//  if (leftTicks < 300) {
+//    motorLeft(127);
+//  } else if (leftTicks > 310){
+//    motorLeft(-127);
+//  } else {
+//    motorLeft(0);
+//  }
+}
+
+int updateLeft(int command, int targetValue, int currentValue) {
+  int leftError = targetValue - currentValue;
+  leftI += leftError - lastLeftError;
+  float pidTerm = (Kp * leftError) + (Kd * (leftError - lastLeftError)) + (Ki * leftI);
+  lastLeftError = leftError;
+  int out = constrain(command+int(pidTerm), -256, 255);
+  if (abs(out) < 40) {
+    return 0;
   } else {
-    motorRight(0);
+    return out;
   }
-  if (leftTicks < 300) {
-    motorLeft(127);
-  } else if (leftTicks > 310){
-    motorLeft(-127);
+}
+
+int updateRight(int command, int targetValue, int currentValue) {
+  int rightError = targetValue - currentValue;
+  rightI += rightError - lastRightError;
+  float pidTerm = (Kp * rightError) + (Kd * (rightError - lastRightError)) +  (Ki * rightI);
+  lastRightError = rightError;
+  int out = constrain(command+int(pidTerm), -255, 255);
+  if (abs(out) < 40) {
+    return 0;
   } else {
-    motorLeft(0);
+    return out;
   }
-  delay(20);
 }
 
 void onLeftTick() {
-  if (digitalRead(LE2_PIN) == 0) {
+  if (digitalRead(LE2_PIN) == LOW) {
     leftTicks++;
   } else {
     leftTicks--;
   }
-//      Serial.println(rightTicks);
-
 }
 
 void onRightTick() {
-  if (digitalRead(RE2_PIN) == 0) {
+  if (digitalRead(RE2_PIN) == LOW ) {
     rightTicks--;
   } else {
     rightTicks++;
   }
-//      Serial.println(rightTicks);
+}
 
+void motorLeft(int spd, bool reversed) {
+  if (spd > 0) {
+    analogWrite(LMPWM_PIN, spd);
+    if (reversed) {
+      digitalWrite(LMF_PIN, HIGH);
+      digitalWrite(LMR_PIN, LOW);
+    } else {
+      digitalWrite(LMF_PIN, LOW);
+      digitalWrite(LMR_PIN, HIGH);
+    }
+  } else {
+    analogWrite(LMPWM_PIN, 0);
+    digitalWrite(LMF_PIN, LOW);
+    digitalWrite(LMR_PIN, LOW);
+  }
 }
 
 void motorLeft(int spd) {
   if (spd > 0) {
-    analogWrite(LMF_PIN, spd);
-    analogWrite(LMR_PIN, 0);
+    analogWrite(LMPWM_PIN, spd);
+    digitalWrite(LMF_PIN, HIGH);
+    digitalWrite(LMR_PIN, LOW);
   } else if (spd < 0) {
-    analogWrite(LMF_PIN, 0);
-    analogWrite(LMR_PIN, -spd);
+    analogWrite(LMPWM_PIN, -spd);
+    digitalWrite(LMF_PIN, LOW);
+    digitalWrite(LMR_PIN, HIGH);
   } else {
-    analogWrite(LMF_PIN, 0);
-    analogWrite(LMR_PIN, 0);
+    analogWrite(LMPWM_PIN, 0);
+    digitalWrite(LMF_PIN, LOW);
+    digitalWrite(LMR_PIN, LOW);
+  }
+}
+
+void motorRight(int spd, bool reversed) {
+  if (spd > 0) {
+    analogWrite(RMPWM_PIN, spd);
+    if (reversed) {
+      digitalWrite(RMF_PIN, HIGH);
+      digitalWrite(RMR_PIN, LOW);
+    } else {
+      digitalWrite(RMF_PIN, LOW);
+      digitalWrite(RMR_PIN, HIGH);
+    }
+  } else {
+    analogWrite(RMPWM_PIN, 0);
+    digitalWrite(RMF_PIN, LOW);
+    digitalWrite(RMR_PIN, LOW);
   }
 }
 
 void motorRight(int spd) {
   if (spd > 0) {
-    analogWrite(RMF_PIN, spd);
-    analogWrite(RMR_PIN, 0);
+    analogWrite(RMPWM_PIN, spd);
+    digitalWrite(RMF_PIN, HIGH);
+    digitalWrite(RMR_PIN, LOW);
   } else if (spd < 0) {
-    analogWrite(RMF_PIN, 0);
-    analogWrite(RMR_PIN, -spd);
+    analogWrite(RMPWM_PIN, -spd);
+    digitalWrite(RMF_PIN, LOW);
+    digitalWrite(RMR_PIN, HIGH);
   } else {
-    analogWrite(RMF_PIN, 0);
-    analogWrite(RMR_PIN, 0);
+    analogWrite(RMPWM_PIN, 0);
+    digitalWrite(RMF_PIN, LOW);
+    digitalWrite(RMR_PIN, LOW);
   }
 }
